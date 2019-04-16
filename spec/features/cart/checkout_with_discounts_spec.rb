c37623332195f47674941dc31c672174f4c45da8
Matt Levy <milevy1@gmail.com>
2019-04-16 11:29:45 -0600

    Added spec for checking out with discounts

---
+++ spec/features/cart/checkout_with_discounts_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+include ActionView::Helpers::NumberHelper
+
+RSpec.describe "Checking out with a bulk discount" do
+  before :each do
+    @merchant_1 = create(:merchant)
+    @discount_1 = @merchant_1.discounts.create(name: "An arbitrary value", discount: 5, threshold: 30)
+    @item_1 = create(:item, user: @merchant_1, inventory: 3, price: 10)
+
+    @merchant_2 = create(:merchant)
+    @item_2 = create(:item, user: @merchant_2)
+
+    visit item_path(@item_1)
+    click_on "Add to Cart"
+    visit item_path(@item_1)
+    click_on "Add to Cart"
+    visit item_path(@item_1)  # Cart has 3x quantity of @item_1 ($30)
+    click_on "Add to Cart"
+    visit item_path(@item_2)  # Cart has 1x quantity of @item_2
+    click_on "Add to Cart"
+  end
+
+  it "updates the cart total to include the discount" do
+    total = @item_1.price * 3 - @discount_1.discount + @item_2.price
+
+    expect(page).to have_content("Total: $#{total}")
+  end
+
+  it "shows the discount details next to the item it applied to" do
+    expected = "Discount: -#{number_to_currency @discount_1.discount} (#{number_to_currency @discount_1.discount} off order of #{number_to_currency @discount_1.threshold} or more)"
+
+    within "#item-#{@item_1.id}" do
+      expect(page).to have_content(expected)
+    end
+  end
+
+  it "updates the item subtotal to include the discount" do
+    expected_subtotal = @item_1.price * 3 - @discount_1.discount
+
+    within "#item-#{@item_1.id}" do
+      expect(page).to have_content("subtotal: #{number_to_currency expected_subtotal}")
+    end
+  end
+end
