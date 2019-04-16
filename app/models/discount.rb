968241e1dae3dbc6a7f9928a62a24e91d2f25d16
Matt Levy <milevy1@gmail.com>
2019-04-16 17:08:06 -0600

    Added validation for :threshold_larger_than_discount

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -5,4 +5,12 @@ class Discount < ApplicationRecord
 
   validates :discount, presence: true, numericality: { greater_than: 0 }
   validates :threshold, presence: true, numericality: { greater_than: 0 }
+
+  validate :threshold_larger_than_discount
+
+  def threshold_larger_than_discount
+    if self.threshold && self.discount && (self.discount >= self.threshold)
+      self.errors.add(:invalid_threshold, "- Threshold value must be greater than the discount value.")
+    end
+  end
 end
